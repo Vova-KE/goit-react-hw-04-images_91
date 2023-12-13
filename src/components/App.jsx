3ecb49b84513c10977b3315d4e4f81beba59eae6
Volodymyr Kozyr <vroger4@gmail.com>
2023-12-13 15:14:53 +0200

    upd folder API
    upd folder API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import imageAPI from './service/image-api';
+import imageAPI from '../service/image-api';
 
 import Button from './Button';
 import ImageGallery from './ImageGallery';
